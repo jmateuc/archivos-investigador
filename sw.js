8dc21ea33a788fd8a38aeb61426f61cae9083157
jmateuc <jaime.mateuc@gmail.com>
2026-08-19 18:02:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='investigador-v3';
+const CACHE='investigador-v4';
 const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./adventures/la-senal/story.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
